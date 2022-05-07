f12219289937ca798749f00699b48b394a0e4f5f
DuctFisherman <quinonezjesus88@gmail.com>
2022-05-07 15:23:16 -0700

    admin check

--- login.php
+++ login.php
@@ -22,8 +22,32 @@ session_start();
     	$genericErrorMsg = "Invalid username and/or password";
 		if ($validation->fetch() && password_verify($password, $res_password)) 
 		{
-				$_SESSION['user_name'] = $user_data['Username'];
+			
+				$_SESSION['user_name'] = $res_user;
 				$_SESSION['loggedIn'] = true; //Sets logged in to true if username and password are correct
+				$_SESSION["is_admin"] = false;
+				$validation->close();
+
+				//checks if user is an admin
+				//if they are they will be redirected to the admin page
+				$isAdminStatement = $con->prepare("CALL IsAdmin(?)");
+				if (!$isAdminStatement) {
+					echo "ERROR!";
+				}
+				$isAdminStatement->bind_param('s', $res_user);
+				$isAdminStatement->execute();
+				mysqli_stmt_bind_result($isAdminStatement, $res_admin);
+				$isAdminStatement->fetch();
+
+				$_SESSION["is_admin"] = $res_admin;
+				
+				if($res_admin)
+				{
+					header('Location: admin.php');
+					die;
+				}
+				
+				
 				header("Location: index.php");
 				die;			
 		}
