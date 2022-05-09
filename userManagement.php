ae52eff0b6ebba8088ed295471587bb07719f919
loporean <84533280+loporean@users.noreply.github.com>
2022-05-09 15:57:37 -0700

    user management

---
+++ userManagement.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+INCLUDE("connection.php");
+INCLUDE("functions.php");
+
+/*if($_SESSION['loggedIn']){
+//allows user access to page if they are confirmed to be logged in
+}
+else{
+//redirect to the login page
+header('Location: login.php'); 
+}*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catches</title>
+    
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.2/css/fixedHeader.bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css">
+
+</head>
+
+<body>
+    <br>
+    <div class="container">
+    <table id="example" class="table table-striped table-bordered nowrap" style="width:100%">
+    <a href="admin.php">Return</a><br>
+    <h1>User Management</h1>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Type</th>
+                </tr>
+        </thead>
+        <tbody>
+            <?php
+            date_default_timezone_set('America/Los_Angeles');
+            error_reporting(E_ALL);
+            ini_set("log_errors", 1);
+            ini_set("display_errors", 1);
+
+            $result = mysqli_query($con,"SELECT Fisherman.Name as FLname, Fisherman.Username, FishermanType.Type as FishermanType 
+            FROM Fisherman inner join FishermanType on Fisherman.Username = FishermanType.Username"); 
+
+
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['FLname'] . "</td>";
+            echo "<td>" . $row['Username'] . "</td>";
+            echo "<td>" . $row['FishermanType'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";
+
+            ?>
+
+        </div>
+
+        </tbody>
+
+    </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/fixedheader/3.2.2/js/dataTables.fixedHeader.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            var table = $('#example').DataTable( {
+                responsive: true
+            } );
+        
+            new $.fn.dataTable.FixedHeader( table );
+        } );
+    </script>
+    <script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
+
+</body>
+</html>
