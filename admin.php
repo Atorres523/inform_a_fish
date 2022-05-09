8bee12e4fbe44147ff4035f90b40b841fd8d5a70
loporean <84533280+loporean@users.noreply.github.com>
2022-05-09 13:54:56 -0700

    added code to restrict someone from changing address to admin page

--- admin.php
+++ admin.php
@@ -3,6 +3,15 @@ session_start(); //checks if the user is logged in
 
 	include("connection.php");
 	include("functions.php");
+
+	if($_SESSION['loggedIn']){
+		//allows user access to page if they are confirmed to be logged in
+		}
+		else{
+		//redirect to the login page
+		header('Location: login.php'); 
+		}
+
     echo"hello";
 
 	//$user_data = check_login($con);
